f2eb36e440eedb775178357ef9caa4a4ac426a4a
brunomrcemi <brunomrcemi@hotmail.com>
2014-11-24 08:59:56 -0200

    2014240859
    Correção SCRIPT

--- js/curriculo.js
+++ js/curriculo.js
@@ -3,11 +3,13 @@
 		 var i;
 		 for(i=0;i <data.length;i++)
 		 {
-		    $('.list-group').append("<a href='" + data[i].link_github +"' class='list-group-item'>" +
+             if(data[i].id != 9)
+             {
+		       $('.list-group').append("<a href='" + data[i].link_html +"' class='list-group-item'>" +
 		                            "<h4 class='list-group-item-heading'>"+ data[i].nome +"</h4>" +
 		                            "<p class='list-group-item-text'>" + data[i].mini_curriculo +"</p>" +
 								    "</a>");
-		 
+		     } 
 		 } 
 		});
 	})
