3ae338fa41e618384588a254326507063b755ba4
brunomrcemi <brunomrcemi@hotmail.com>
2014-11-25 00:24:21 -0200

    2014250024
    Correção

--- js/curriculo.js
+++ js/curriculo.js
@@ -1,9 +1,12 @@
  $('document').ready(function(){
         $.getJSON('http://rarolabs.com.br:88/alunos.json', function(data){
 		 var i;
+		 
+		$('.list-group:first-child').find('span').remove();
+		
 		 for(i=0;i <data.length;i++)
 		 {
-		    $('.list-group').append("<a href='" + data[i].link_github +"' class='list-group-item'>" +
+		    $('.list-group').append("<a href='" + data[i].link_html +"' class='list-group-item'>" +
 		                            "<h4 class='list-group-item-heading'>"+ data[i].nome +"</h4>" +
 		                            "<p class='list-group-item-text'>" + data[i].mini_curriculo +"</p>" +
 								    "</a>");
