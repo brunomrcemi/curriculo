8406ab695abc21c6d5fe9d16f34ae69902318b7d
brunomrcemi <brunomrcemi@hotmail.com>
2014-11-25 00:33:49 -0200

    201425110033
    Correção

--- js/curriculo.js
+++ js/curriculo.js
@@ -1,15 +1,16 @@
  $('document').ready(function(){
         $.getJSON('http://rarolabs.com.br:88/alunos.json', function(data){
 		 var i;
+		 
+		$('.list-group:first-child').find('span').remove();
+		
 		 for(i=0;i <data.length;i++)
 		 {
-             if(data[i].id != 9)
-             {
-		       $('.list-group').append("<a href='" + data[i].link_html +"' class='list-group-item'>" +
+		    $('.list-group').append("<a href='" + data[i].link_html +"' class='list-group-item'>" +
 		                            "<h4 class='list-group-item-heading'>"+ data[i].nome +"</h4>" +
 		                            "<p class='list-group-item-text'>" + data[i].mini_curriculo +"</p>" +
 								    "</a>");
-		     } 
+		 
 		 } 
 		});
 	})
